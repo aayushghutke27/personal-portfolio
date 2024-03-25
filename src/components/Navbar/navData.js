e3e659b52ead04c2227e142b2b8261893c0d597c
Ayush Ghutke <aayushghutke@gmail.com>
2024-03-25 23:51:05 +0530

    first commit

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,52 @@
+export const navLinks = [
+
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+
+  
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+ 
+
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+
+  {
+    label: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/aayushghutke27',
+    icon: 'link',
+  },
+ 
+  {
+    label: 'Behance',
+    url: 'https://www.behance.net/aayushghutke27',
+    icon: 'behance',
+  },
+
+  {
+    label: 'Dribbble',
+    url: 'https://dribbble.com/aayushghutke27',
+    icon: 'dribbble',
+  },
+
+  {
+    label: 'Figma',
+    url: 'https://www.figma.com/@ayushghutke',
+    icon: 'figma',
+  },
+
+  //{
+  //  label: 'Github',
+  //  url: 'https://github.com/aayushghutke27',
+  //  icon: 'github',
+  //},
+];
